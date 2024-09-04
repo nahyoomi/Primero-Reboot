817e2f258588ebee0fd342a46f4256da559bc142
nahyoomi <nahyomi2000@gmail.com>
2024-09-04 16:01:14 +0100

    Commit segunda parte

--- git-and-github-practice/script.js
+++ git-and-github-practice/script.js
@@ -3,3 +3,6 @@
 // cual es el cafe mas peligroso?
 
 // el EXPreso jaja 
+
+//Gracias Yanina, sabes porque los programadores no cuentan chistes?
+// Porque sino se quedarian en un loop inifinito explicando el chiste
